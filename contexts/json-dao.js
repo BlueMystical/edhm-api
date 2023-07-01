03f0135dbec49fa7df1e85f13d104b9cee22c8dd
BlueMystic <jhollman.chacon@gmail.com>
2023-07-01 13:17:02 -0300

    0.7

--- contexts/json-dao.js
+++ contexts/json-dao.js
@@ -152,9 +152,11 @@ exports.JSONDB_GetUsers = async function () {
         //console.log(ret);
         //_Response.result.push(ret.props);
 
-        var ret = await edhm_users.item(user.key).get();
-        console.log(ret);
+        var item = await edhm_users.item(user.key);
+        console.log(item);
+        var ret = await item.get();
         _Response.result.push(ret.props);
+        
         //_Response.result.push(user);
 
         /*let me = await user.get().then(ret => {
